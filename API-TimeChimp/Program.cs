170fe85955a56a85369267a8761b35bfb48643e4
student2 <student2@DEVIONSOFTWARE>
2023-07-26 10:54:27 +0200

    Added Employee interactions

--- API-TimeChimp/Program.cs
+++ API-TimeChimp/Program.cs
@@ -42,6 +42,12 @@ app.MapPost("/api/devion/project", (ProjectTimeChimp project) => TimeChimpProjec
 
 app.MapPut("/api/devion/project", (ProjectTimeChimp project) => TimeChimpProjectHelper.UpdateProject(project)).WithName("UpdateProject");
 
+app.MapGet("/api/devion/employees", () => TimeChimpEmployeeHelper.GetEmployees()).WithName("GetEmployees");
+
+app.MapPost("/api/devion/employee", (EmployeeTimeChimp employee) => TimeChimpEmployeeHelper.CreateEmployee(employee)).WithName("CreateEmployee");
+
+app.MapPut("/api/devion/employee", (EmployeeTimeChimp employee) => TimeChimpEmployeeHelper.UpdateEmployee(employee)).WithName("UpdateEmployee");
+
 app.MapGet("api/devion/times", () =>
 {
     // connection with timechimp
@@ -96,4 +102,4 @@ app.MapGet("api/devion/times", () =>
 .WithName("GetTimesFromLastWeek");
 
 
-app.Run("http://localhost:5001");
+app.Run();
