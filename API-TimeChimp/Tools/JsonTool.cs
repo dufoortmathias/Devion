951596bc5cf00be9b7ce3d3be8fb6cb4df72b043
dufoortmathias <91429013+dufoortmathias@users.noreply.github.com>
2023-08-09 11:29:51 +0200

    feat: added comments to jsonTool.cs

--- API-TimeChimp/Tools/JsonTool.cs
+++ API-TimeChimp/Tools/JsonTool.cs
@@ -4,17 +4,19 @@ namespace Api.Devion.Tools
 {
     public static class JsonTool
     {
+        //convert to object
         public static T ConvertTo<T>(String json)
         {
-            Console.WriteLine(json);
             return JsonConvert.DeserializeObject<T>(json);
         }
 
+        //convert to string without null values
         public static String ConvertFrom(Object? obj)
         {
             return JsonConvert.SerializeObject(obj, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
         }
 
+        //convert to string with null values
         public static String ConvertFromWithNullValues(Object? obj)
         {
             return JsonConvert.SerializeObject(obj);
