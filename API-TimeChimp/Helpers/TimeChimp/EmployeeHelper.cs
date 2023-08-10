1f821bf758df15fa32027cc71e889b872c7c8201
dufoortmathias <91429013+dufoortmathias@users.noreply.github.com>
2023-08-10 09:50:19 +0200

    feat: added errorhandling employeehelper.cs timechimp

--- API-TimeChimp/Helpers/TimeChimp/EmployeeHelper.cs
+++ API-TimeChimp/Helpers/TimeChimp/EmployeeHelper.cs
@@ -16,10 +16,16 @@ namespace Api.Devion.Helpers.TimeChimp
         public List<EmployeeTimeChimp> GetEmployees()
         {
             //get data from timechimp
-            String response = TCClient.GetAsync("v1/users").Result;
+            var response = TCClient.GetAsync("v1/users");
+
+            //check if response is succesfull
+            if (!response.IsCompletedSuccessfully)
+            {
+                throw new Exception("Error getting all employees from timechimp with endpoint: v1/users");
+            }
 
             //convert data to employeeTimeChimp object
-            List<EmployeeTimeChimp> employees = JsonTool.ConvertTo<List<EmployeeTimeChimp>>(response);
+            List<EmployeeTimeChimp> employees = JsonTool.ConvertTo<List<EmployeeTimeChimp>>(response.Result);
             return employees;
         }
 
@@ -33,11 +39,23 @@ namespace Api.Devion.Helpers.TimeChimp
             //create json
             String json = JsonTool.ConvertFrom(employee);
 
+            //check if json is not empty
+            if (json == null)
+            {
+                throw new Exception("Error converting employee to json");
+            }
+
             //send data to timechimp
-            String response = TCClient.PostAsync($"v1/users", json).Result;
+            var response = TCClient.PostAsync($"v1/users", json);
+
+            //check if response is succesfull
+            if (!response.IsCompletedSuccessfully)
+            {
+                throw new Exception("Error creating employee in timechimp with endpoint: v1/users");
+            }
 
             //convert response to employeeTimeChimp object
-            EmployeeTimeChimp employeeResponse = JsonTool.ConvertTo<EmployeeTimeChimp>(response);
+            EmployeeTimeChimp employeeResponse = JsonTool.ConvertTo<EmployeeTimeChimp>(response.Result);
             return employeeResponse;
         }
 
@@ -50,11 +68,23 @@ namespace Api.Devion.Helpers.TimeChimp
             //create json
             String json = JsonTool.ConvertFrom(employee);
 
+            //check if json is not empty
+            if (json == null)
+            {
+                throw new Exception("Error converting employee to json");
+            }
+
             //send data to timechimp
-            String response = TCClient.PutAsync("v1/users", json).Result;
+            var response = TCClient.PutAsync("v1/users", json);
+
+            //check if response is succesfull
+            if (!response.IsCompletedSuccessfully)
+            {
+                throw new Exception("Error updating employee in timechimp with endpoint: v1/users");
+            }
 
             //convert response to employeeTimeChimp object
-            EmployeeTimeChimp employeeResponse = JsonTool.ConvertTo<EmployeeTimeChimp>(response);
+            EmployeeTimeChimp employeeResponse = JsonTool.ConvertTo<EmployeeTimeChimp>(response.Result);
             return employeeResponse;
         }
 
@@ -62,10 +92,16 @@ namespace Api.Devion.Helpers.TimeChimp
         public EmployeeTimeChimp GetEmployee(Int32 employeeId)
         {
             //get data form timechimp
-            String response = TCClient.GetAsync($"v1/users/{employeeId}").Result;
+            var response = TCClient.GetAsync($"v1/users/{employeeId}");
+
+            //check if response is succesfull
+            if (!response.IsCompletedSuccessfully)
+            {
+                throw new Exception($"Error getting employee from timechimp with endpoint: v1/users/{employeeId}");
+            }
 
             //convert data to employeeTimeChimp object
-            EmployeeTimeChimp employeeResponse = JsonTool.ConvertTo<EmployeeTimeChimp>(response);
+            EmployeeTimeChimp employeeResponse = JsonTool.ConvertTo<EmployeeTimeChimp>(response.Result);
             return employeeResponse;
         }
     }
