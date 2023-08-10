2ff43a360b3f01afecc571dc8582519d98e20dae
dufoortmathias <91429013+dufoortmathias@users.noreply.github.com>
2023-08-10 08:34:50 +0200

    feat: added comment mileagehelper.cs

--- API-TimeChimp/Helpers/ETS/MileageHelper.cs
+++ API-TimeChimp/Helpers/ETS/MileageHelper.cs
@@ -6,22 +6,36 @@ public class ETSMileageHelper : ETSHelper
     {
     }
 
+    //get all mileages
     public List<mileageETS> GetMileages()
     {
+        //get data form ETS
         var response = ETSClient.selectQuery("SELECT PLA_ID, PLA_KM, PLA_PROJECT, PLA_SUBPROJECT, PLA_PERSOON, PLA_KM_DERDEN, PLA_KM_VERGOEDING, PLA_START FROM tbl_planning");
+
+        //convert data to mileageETS object
         List<mileageETS> mileages = JsonTool.ConvertTo<List<mileageETS>>(response);
         return mileages;
     }
 
+    //add a mileage
     public string UpdateMileage(mileageETS mileage)
     {
+        //create query to get projectid and subprojectid for the mileage
         var queryGet = $"SELECT * FROM tbl_planning WHERE PLA_PROJECT = {mileage.PLA_PROJECT} AND PLA_SUBPROJECT = {mileage.PLA_SUBPROJECT};";
+
+        //get data from ETS
         var responseGet = ETSClient.selectQuery(queryGet);
+
+        //convert data to mileageETS object
         List<mileageETS> mileages = JsonTool.ConvertTo<List<mileageETS>>(responseGet);
+
+        //check if there are mileages found
         if (mileages.Count == 0)
         {
             return "No mileage found";
         }
+
+        //set PLA_ID
         foreach (mileageETS mileageETS in mileages)
         {
             if (mileageETS.PLA_START.Date == mileage.PLA_START.Date && mileageETS.PLA_PERSOON == mileage.PLA_PERSOON)
@@ -32,6 +46,8 @@ public class ETSMileageHelper : ETSHelper
 
         //update query
         var query = $"UPDATE tbl_planning SET PLA_KM_HEEN_TERUG = 0, PLA_KM = {mileage.PLA_KM}, PLA_KM_DERDEN = '{mileage.PLA_KM_DERDEN}', PLA_KM_VERGOEDING = '{mileage.PLA_KM_VERGOEDING}' WHERE PLA_ID = {mileage.PLA_ID};";
+
+        //send data to ETS
         var response = ETSClient.updateQuery(query);
         return response;
     }
