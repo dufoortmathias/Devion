1ca1d447622c1223c10e728b0a7aa38ad065e9c7
dufoortmathias <91429013+dufoortmathias@users.noreply.github.com>
2023-08-10 08:54:16 +0200

    feat: added comment bearertokenclient.cs

--- API-TimeChimp/Client/BearerTokenClient.cs
+++ API-TimeChimp/Client/BearerTokenClient.cs
@@ -11,6 +11,7 @@ public class BearerTokenHttpClient : IBearerTokenHttpClient
 {
     private readonly HttpClient _httpClient;
 
+    // Create a new instance of HttpClient that is configured with the base address of the API and the bearer token
     public BearerTokenHttpClient(String url, String token)
     {
         _httpClient = new HttpClient
@@ -21,11 +22,16 @@ public class BearerTokenHttpClient : IBearerTokenHttpClient
         _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
     }
 
+    //get async
     public async Task<string> GetAsync(string endpoint)
     {
+        // Send the request to the API endpoint with response
         HttpResponseMessage response = await _httpClient.GetAsync(endpoint);
+
+        //check if statuscode is success
         if (response.IsSuccessStatusCode)
         {
+            // Return the response body
             return await response.Content.ReadAsStringAsync();
         }
         else
@@ -35,13 +41,19 @@ public class BearerTokenHttpClient : IBearerTokenHttpClient
         }
     }
 
+    //post async
     public async Task<string> PostAsync(string endpoint, string jsonPayload)
     {
+        // Create a StringContent object with the JSON payload
         var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
 
+        //send the request to the API endpoint with response
         HttpResponseMessage response = await _httpClient.PostAsync(endpoint, content);
+
+        //check if statuscode is success
         if (response.IsSuccessStatusCode)
         {
+            // Return the response body
             return await response.Content.ReadAsStringAsync();
         }
         else
@@ -51,13 +63,19 @@ public class BearerTokenHttpClient : IBearerTokenHttpClient
         }
     }
 
+    //put async
     public async Task<string> PutAsync(string endpoint, string jsonPayload)
     {
+        // Create a StringContent object with the JSON payload
         var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
 
+        //send the request to the API endpoint with response
         HttpResponseMessage response = await _httpClient.PutAsync(endpoint, content);
+
+        //check if statuscode is success
         if (response.IsSuccessStatusCode)
         {
+            // Return the response body
             return await response.Content.ReadAsStringAsync();
         }
         else
