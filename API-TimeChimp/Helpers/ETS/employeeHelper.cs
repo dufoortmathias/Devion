27594fedcb2923a7e586ad94bc99e5563fd26520
dufoortmathias <91429013+dufoortmathias@users.noreply.github.com>
2023-08-10 08:37:00 +0200

    feat: added comment employeehelper.cs

--- API-TimeChimp/Helpers/ETS/employeeHelper.cs
+++ API-TimeChimp/Helpers/ETS/employeeHelper.cs
@@ -6,28 +6,43 @@ public class ETSEmployeeHelper : ETSHelper
     {
     }
 
+    //get all employees
     public List<EmployeeETS> GetEmployees()
     {
-        //connection with ETS
+        //get data form ETS
         var response = ETSClient.selectQuery("select * from J2W_PNPX");
+
+        //convert data to employeeETS object
         List<EmployeeETS> employees = JsonTool.ConvertTo<List<EmployeeETS>>(response);
         return employees;
     }
 
+    //get all employeeids that are changed after the given date
     public List<string> GetEmployeeIdsChangedAfter(DateTime date)
     {
+        //create query
         string query = $"SELECT PN_ID FROM J2W_PNPX WHERE DATE_CHANGED >= '{date.ToString("MM/dd/yyyy HH:mm")}' ";
+
+        //get data from ETS
         string json = ETSClient.selectQuery(query);
+
+        //get all ids from the json
         List<string> ids = JsonTool.ConvertTo<List<EmployeeETS>>(json)
             .Select(employee => employee.PN_ID)
             .ToList();
         return ids;
     }
 
+    //get employee by employeeId
     public EmployeeETS GetEmployee(String employeeId)
     {
+        //create query
         string query = $"select * from J2W_PNPX where PN_ID = {employeeId}";
+
+        //get data from ETS
         String json = ETSClient.selectQuery(query);
+
+        //convert data to employeeETS object
         EmployeeETS employee = JsonTool.ConvertTo<EmployeeETS[]>(json).FirstOrDefault();
         return employee;
     }
