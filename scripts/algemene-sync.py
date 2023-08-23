452852ac8c952d70db5e12debbcd613d77b38c83
student2 <student2@DEVIONSOFTWARE>
2023-08-23 09:23:49 +0200

    change launch ip address

--- scripts/algemene-sync.py
+++ scripts/algemene-sync.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
 ## Start time script
 start_time = datetime.datetime.now()
 ## Base URL API
-base_URL = f"https://localhost:7223/api/{company}/"
+base_URL = f"http://localhost:5142/api/{company}/"
 ## Name of data file that stores information about last time this script 
 data_filename = f"data_{company}.json"
 ## Defines format of how datetime objects should be tranlated to a String
