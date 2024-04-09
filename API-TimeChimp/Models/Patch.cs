5f24417cf56b603ecc3583cf537df86540233b9d
fourke <mathias.dufoort3@gmail.com>
2024-04-09 15:15:41 +0200

    fix uurcode

---
+++ API-TimeChimp/Models/Patch.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Api.Devion.Models;
+public class Patch
+{
+    public string? Op { get; set; }
+    public string? Path { get; set; }
+    public string? Value { get; set; }
+    public Patch(string op,string path,string value)
+    {
+        Op = op;
+        Path = path;
+        Value = value;
+    }
+}
